d6bcb9e3cf5c8d93bade139d005170597d0c477d
GlinZachariah <glincool.zac@gmail.com>
2019-03-15 16:00:07 +0530

    added fever context

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
 from flask import Flask
-from flask import *
+from flask import request
+from flask import make_response
+from flask import jsonify
+
 import disease
 
 app = Flask(__name__)
+
+param ={"parameters": {
+          "response": "no",
+          "response.original": "bad"
+        }}
+
+def final_format_reply(msg,sname,name):
+    reponse ={
+            "fulfillmentText": ""+msg+"",
+            "outputContexts": [
+                {
+                "name": ""+sname+name+"",
+                "lifespanCount": 1,
+                "parameters": {
+                    "param": "param value"
+                }
+                }
+            ]
+            }
+    return reponse
 
 
 @app.route('/')
@@ -12,11 +35,20 @@ def hello_world():
 def results():
     message=""
     req = request.get_json(force=True)
-    action = req.get('queryResult').get('action')
+    # action = req.get('queryResult').get('action')
     intent_name = req.get('queryResult').get('intent').get('displayName')
-    last_intent= intent_name
+    session_name = req.get('session')
+    # last_intent= intent_name
     if intent_name == "iswell":
-        message = disease.symptoms(req)
+          print("is well intent")
+          name ="/contexts/isfever"
+          message = disease.symptoms(req)
+          return final_format_reply(message,session_name,name)
+    if intent_name == "isfever":
+          print("is well intent")
+          name ="/contexts/isheadache"
+          message = disease.symptoms(req)
+          return final_format_reply(message,session_name,name)
     return {'fulfillmentText' : message}
 
 @app.route('/paleo',methods=['POST'])
--- disease.py
+++ disease.py
@@ -3,5 +3,7 @@ import json
 def symptoms(req):
     parameter_list = req.get('queryResult').get('parameters')
     print(parameter_list['response'])
-    message="None"
+    message="Are you having Fever?"
     return message
+
+ 
