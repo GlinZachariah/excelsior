812ab6729a469c019bc60785a419cea8f47c3b53
GlinZachariah <glincool.zac@gmail.com>
2019-03-15 11:29:27 +0530

    added disease base file

--- app.py
+++ app.py
@@ -8,6 +8,17 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+def results():
+    message=""
+    req = request.get_json(force=True)
+    action = req.get('queryResult').get('action')
+    intent_name = req.get('queryResult').get('intent').get('displayName')
+    last_intent= intent_name
+    return {'fulfillmentText' : "message"}
+
+@app.route('/paleo',methods=['POST'])
+def paleo():
+    return make_response(jsonify(results()))
 
 if __name__ == '__main__':
     app.run()
